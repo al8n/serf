85d15fe94a003cc34f39340147684c2cbe23f070
al8n <scygliu1@gmail.com>
2025-03-04 23:34:15 +0800

    Add more unit tests

--- serf-core/src/coalesce/member.rs
+++ serf-core/src/coalesce/member.rs
@@ -112,286 +112,281 @@ where
   }
 }
 
-// #[cfg(all(test, feature = "test"))]
-// #[allow(clippy::collapsible_match)]
-// mod tests {
-//   use std::{net::SocketAddr, time::Duration};
+#[cfg(all(test, feature = "test"))]
+#[allow(clippy::collapsible_match)]
+mod tests {
+  use std::{net::SocketAddr, time::Duration};
 
-//   use futures::FutureExt;
-//   use memberlist_core::{
-//     agnostic_lite::{RuntimeLite, tokio::TokioRuntime},
-//     transport::resolver::socket_addr::SocketAddrResolver,
-//   };
-//   use crate::types::{MemberStatus, UserEventMessage};
-//   use smol_str::SmolStr;
+  use crate::types::{MemberStatus, UserEventMessage};
+  use futures::FutureExt;
+  use memberlist_core::{
+    agnostic_lite::{RuntimeLite, tokio::TokioRuntime},
+    transport::{resolver::socket_addr::SocketAddrResolver, unimplemented::UnimplementedTransport},
+  };
+  use smol_str::SmolStr;
 
-//   use crate::{
-//     DefaultDelegate,
-//     coalesce::coalesced_event,
-//     event::{CrateEventType, MemberEvent},
-//   };
+  use crate::{
+    DefaultDelegate,
+    coalesce::coalesced_event,
+    event::{CrateEventType, MemberEvent},
+  };
 
-//   use super::*;
+  use super::*;
 
-//   type Transport = UnimplementedTransport<
-//     SmolStr,
-//     SocketAddrResolver<TokioRuntime>,
-//
-//     TokioRuntime,
-//   >;
+  type Transport = UnimplementedTransport<SmolStr, SocketAddrResolver<TokioRuntime>, TokioRuntime>;
 
-//   type Delegate = DefaultDelegate<Transport>;
+  type Delegate = DefaultDelegate<Transport>;
 
-//   #[tokio::test]
-//   async fn test_member_event_coealesce_basic() {
-//     let (tx, rx) = async_channel::unbounded();
-//     let (_shutdown_tx, shutdown_rx) = async_channel::bounded(1);
-//     let coalescer = MemberEventCoalescer::<Transport, Delegate>::new();
+  #[tokio::test]
+  async fn test_member_event_coealesce_basic() {
+    let (tx, rx) = async_channel::unbounded();
+    let (_shutdown_tx, shutdown_rx) = async_channel::bounded(1);
+    let coalescer = MemberEventCoalescer::<Transport, Delegate>::new();
 
-//     let in_ = coalesced_event(
-//       tx,
-//       shutdown_rx,
-//       Duration::from_millis(20),
-//       Duration::from_millis(20),
-//       coalescer,
-//     );
+    let in_ = coalesced_event(
+      tx,
+      shutdown_rx,
+      Duration::from_millis(20),
+      Duration::from_millis(20),
+      coalescer,
+    );
 
-//     let send = vec![
-//       MemberEvent {
-//         ty: MemberEventType::Join,
-//         members: TinyVec::from(Member::new(
-//           Node::new("foo".into(), "127.0.0.1:8080".parse().unwrap()),
-//           Default::default(),
-//           MemberStatus::None,
-//         ))
-//         .into(),
-//       },
-//       MemberEvent {
-//         ty: MemberEventType::Leave,
-//         members: TinyVec::from(Member::new(
-//           Node::new("foo".into(), "127.0.0.1:8080".parse().unwrap()),
-//           Default::default(),
-//           MemberStatus::None,
-//         ))
-//         .into(),
-//       },
-//       MemberEvent {
-//         ty: MemberEventType::Leave,
-//         members: TinyVec::from(Member::new(
-//           Node::new("bar".into(), "127.0.0.1:8080".parse().unwrap()),
-//           Default::default(),
-//           MemberStatus::None,
-//         ))
-//         .into(),
-//       },
-//       MemberEvent {
-//         ty: MemberEventType::Update,
-//         members: TinyVec::from(Member::new(
-//           Node::new("zip".into(), "127.0.0.1:8080".parse().unwrap()),
-//           [("role", "foo")].into_iter().collect(),
-//           MemberStatus::None,
-//         ))
-//         .into(),
-//       },
-//       MemberEvent {
-//         ty: MemberEventType::Update,
-//         members: TinyVec::from(Member::new(
-//           Node::new("zip".into(), "127.0.0.1:8080".parse().unwrap()),
-//           [("role", "bar")].into_iter().collect(),
-//           MemberStatus::None,
-//         ))
-//         .into(),
-//       },
-//       MemberEvent {
-//         ty: MemberEventType::Reap,
-//         members: TinyVec::from(Member::new(
-//           Node::new("dead".into(), "127.0.0.1:8080".parse().unwrap()),
-//           Default::default(),
-//           MemberStatus::None,
-//         ))
-//         .into(),
-//       },
-//     ];
+    let send = vec![
+      MemberEvent {
+        ty: MemberEventType::Join,
+        members: TinyVec::from(Member::new(
+          Node::new("foo".into(), "127.0.0.1:8080".parse().unwrap()),
+          Default::default(),
+          MemberStatus::None,
+        ))
+        .into(),
+      },
+      MemberEvent {
+        ty: MemberEventType::Leave,
+        members: TinyVec::from(Member::new(
+          Node::new("foo".into(), "127.0.0.1:8080".parse().unwrap()),
+          Default::default(),
+          MemberStatus::None,
+        ))
+        .into(),
+      },
+      MemberEvent {
+        ty: MemberEventType::Leave,
+        members: TinyVec::from(Member::new(
+          Node::new("bar".into(), "127.0.0.1:8080".parse().unwrap()),
+          Default::default(),
+          MemberStatus::None,
+        ))
+        .into(),
+      },
+      MemberEvent {
+        ty: MemberEventType::Update,
+        members: TinyVec::from(Member::new(
+          Node::new("zip".into(), "127.0.0.1:8080".parse().unwrap()),
+          [("role", "foo")].into_iter().collect(),
+          MemberStatus::None,
+        ))
+        .into(),
+      },
+      MemberEvent {
+        ty: MemberEventType::Update,
+        members: TinyVec::from(Member::new(
+          Node::new("zip".into(), "127.0.0.1:8080".parse().unwrap()),
+          [("role", "bar")].into_iter().collect(),
+          MemberStatus::None,
+        ))
+        .into(),
+      },
+      MemberEvent {
+        ty: MemberEventType::Reap,
+        members: TinyVec::from(Member::new(
+          Node::new("dead".into(), "127.0.0.1:8080".parse().unwrap()),
+          Default::default(),
+          MemberStatus::None,
+        ))
+        .into(),
+      },
+    ];
 
-//     for event in send {
-//       in_.send(CrateEvent::from(event)).await.unwrap();
-//     }
+    for event in send {
+      in_.send(CrateEvent::from(event)).await.unwrap();
+    }
 
-//     let mut events = HashMap::new();
-//     let timeout = TokioRuntime::sleep(Duration::from_millis(40));
-//     futures::pin_mut!(timeout);
-//     loop {
-//       futures::select! {
-//         e = rx.recv().fuse() => {
-//           let e = e.unwrap();
-//           events.insert(e.ty(), e.clone());
-//         }
-//         _ = (&mut timeout).fuse() => {
-//           break;
-//         },
-//       }
-//     }
+    let mut events = HashMap::new();
+    let timeout = TokioRuntime::sleep(Duration::from_millis(40));
+    futures::pin_mut!(timeout);
+    loop {
+      futures::select! {
+        e = rx.recv().fuse() => {
+          let e = e.unwrap();
+          events.insert(e.ty(), e.clone());
+        }
+        _ = (&mut timeout).fuse() => {
+          break;
+        },
+      }
+    }
 
-//     assert_eq!(events.len(), 3);
+    assert_eq!(events.len(), 3);
 
-//     match events.get(&CrateEventType::Member(MemberEventType::Leave)) {
-//       None => panic!(""),
-//       Some(e) => match e {
-//         CrateEvent::Member(MemberEvent { members, .. }) => {
-//           assert_eq!(members.len(), 2);
+    match events.get(&CrateEventType::Member(MemberEventType::Leave)) {
+      None => panic!(""),
+      Some(e) => match e {
+        CrateEvent::Member(MemberEvent { members, .. }) => {
+          assert_eq!(members.len(), 2);
 
-//           let expected = ["bar", "foo"];
-//           let mut names = [members[0].node.id().clone(), members[1].node.id().clone()];
-//           names.sort();
+          let expected = ["bar", "foo"];
+          let mut names = [members[0].node.id().clone(), members[1].node.id().clone()];
+          names.sort();
 
-//           assert_eq!(names, expected);
-//         }
-//         _ => panic!(""),
-//       },
-//     }
+          assert_eq!(names, expected);
+        }
+        _ => panic!(""),
+      },
+    }
 
-//     match events.get(&CrateEventType::Member(MemberEventType::Update)) {
-//       None => panic!(""),
-//       Some(e) => match e {
-//         CrateEvent::Member(MemberEvent { members, .. }) => {
-//           assert_eq!(members.len(), 1);
-//           assert_eq!(members[0].node.id(), "zip");
-//           assert_eq!(members[0].tags().get("role").unwrap(), "bar");
-//         }
-//         _ => panic!(""),
-//       },
-//     }
+    match events.get(&CrateEventType::Member(MemberEventType::Update)) {
+      None => panic!(""),
+      Some(e) => match e {
+        CrateEvent::Member(MemberEvent { members, .. }) => {
+          assert_eq!(members.len(), 1);
+          assert_eq!(members[0].node.id(), "zip");
+          assert_eq!(members[0].tags().get("role").unwrap(), "bar");
+        }
+        _ => panic!(""),
+      },
+    }
 
-//     match events.get(&CrateEventType::Member(MemberEventType::Reap)) {
-//       None => panic!(""),
-//       Some(e) => match e {
-//         CrateEvent::Member(MemberEvent { members, .. }) => {
-//           assert_eq!(members.len(), 1);
-//           assert_eq!(members[0].node.id(), "dead");
-//         }
-//         _ => panic!(""),
-//       },
-//     }
-//   }
+    match events.get(&CrateEventType::Member(MemberEventType::Reap)) {
+      None => panic!(""),
+      Some(e) => match e {
+        CrateEvent::Member(MemberEvent { members, .. }) => {
+          assert_eq!(members.len(), 1);
+          assert_eq!(members[0].node.id(), "dead");
+        }
+        _ => panic!(""),
+      },
+    }
+  }
 
-//   #[tokio::test]
-//   async fn test_member_event_coalesce_tag_update() {
-//     let (tx, rx) = async_channel::unbounded();
-//     let (_shutdown_tx, shutdown_rx) = async_channel::bounded(1);
-//     let coalescer = MemberEventCoalescer::<Transport, Delegate>::new();
+  #[tokio::test]
+  async fn test_member_event_coalesce_tag_update() {
+    let (tx, rx) = async_channel::unbounded();
+    let (_shutdown_tx, shutdown_rx) = async_channel::bounded(1);
+    let coalescer = MemberEventCoalescer::<Transport, Delegate>::new();
 
-//     let in_ = coalesced_event(
-//       tx,
-//       shutdown_rx,
-//       Duration::from_millis(5),
-//       Duration::from_millis(5),
-//       coalescer,
-//     );
+    let in_ = coalesced_event(
+      tx,
+      shutdown_rx,
+      Duration::from_millis(5),
+      Duration::from_millis(5),
+      coalescer,
+    );
 
-//     in_
-//       .send(CrateEvent::from(MemberEvent {
-//         ty: MemberEventType::Update,
-//         members: TinyVec::from(Member::new(
-//           Node::new("foo".into(), "127.0.0.1:8080".parse().unwrap()),
-//           [("role", "foo")].into_iter().collect(),
-//           MemberStatus::None,
-//         ))
-//         .into(),
-//       }))
-//       .await
-//       .unwrap();
+    in_
+      .send(CrateEvent::from(MemberEvent {
+        ty: MemberEventType::Update,
+        members: TinyVec::from(Member::new(
+          Node::new("foo".into(), "127.0.0.1:8080".parse().unwrap()),
+          [("role", "foo")].into_iter().collect(),
+          MemberStatus::None,
+        ))
+        .into(),
+      }))
+      .await
+      .unwrap();
 
-//     TokioRuntime::sleep(Duration::from_millis(30)).await;
+    TokioRuntime::sleep(Duration::from_millis(30)).await;
 
-//     futures::select! {
-//       e = rx.recv().fuse() => {
-//         let e = e.unwrap();
+    futures::select! {
+      e = rx.recv().fuse() => {
+        let e = e.unwrap();
 
-//         match e {
-//           CrateEvent::Member(MemberEvent { ty, .. }) => {
-//             assert!(matches!(ty, MemberEventType::Update));
-//           }
-//           _ => panic!("expected update"),
-//         }
-//       }
-//       default => panic!("expected update"),
-//     }
+        match e {
+          CrateEvent::Member(MemberEvent { ty, .. }) => {
+            assert!(matches!(ty, MemberEventType::Update));
+          }
+          _ => panic!("expected update"),
+        }
+      }
+      default => panic!("expected update"),
+    }
 
-//     // Second update should not be suppressed even though
-//     // last event was an update
-//     in_
-//       .send(CrateEvent::from(MemberEvent {
-//         ty: MemberEventType::Update,
-//         members: TinyVec::from(Member::new(
-//           Node::new("foo".into(), "127.0.0.1:8080".parse().unwrap()),
-//           [("role", "bar")].into_iter().collect(),
-//           MemberStatus::None,
-//         ))
-//         .into(),
-//       }))
-//       .await
-//       .unwrap();
-//     TokioRuntime::sleep(Duration::from_millis(10)).await;
+    // Second update should not be suppressed even though
+    // last event was an update
+    in_
+      .send(CrateEvent::from(MemberEvent {
+        ty: MemberEventType::Update,
+        members: TinyVec::from(Member::new(
+          Node::new("foo".into(), "127.0.0.1:8080".parse().unwrap()),
+          [("role", "bar")].into_iter().collect(),
+          MemberStatus::None,
+        ))
+        .into(),
+      }))
+      .await
+      .unwrap();
+    TokioRuntime::sleep(Duration::from_millis(10)).await;
 
-//     futures::select! {
-//       e = rx.recv().fuse() => {
-//         let e = e.unwrap();
+    futures::select! {
+      e = rx.recv().fuse() => {
+        let e = e.unwrap();
 
-//         match e {
-//           CrateEvent::Member(MemberEvent { ty, .. }) => {
-//             assert!(matches!(ty, MemberEventType::Update));
-//           }
-//           _ => panic!("expected update"),
-//         }
-//       }
-//       default => panic!("expected update"),
-//     }
-//   }
+        match e {
+          CrateEvent::Member(MemberEvent { ty, .. }) => {
+            assert!(matches!(ty, MemberEventType::Update));
+          }
+          _ => panic!("expected update"),
+        }
+      }
+      default => panic!("expected update"),
+    }
+  }
 
-//   #[test]
-//   fn test_member_event_coalesce_pass_through() {
-//     let cases = [
-//       (CrateEvent::from(UserEventMessage::default()), false),
-//       (
-//         CrateEvent::from(MemberEvent {
-//           ty: MemberEventType::Join,
-//           members: TinyVec::new().into(),
-//         }),
-//         true,
-//       ),
-//       (
-//         CrateEvent::from(MemberEvent {
-//           ty: MemberEventType::Leave,
-//           members: TinyVec::new().into(),
-//         }),
-//         true,
-//       ),
-//       (
-//         CrateEvent::from(MemberEvent {
-//           ty: MemberEventType::Failed,
-//           members: TinyVec::new().into(),
-//         }),
-//         true,
-//       ),
-//       (
-//         CrateEvent::from(MemberEvent {
-//           ty: MemberEventType::Update,
-//           members: TinyVec::new().into(),
-//         }),
-//         true,
-//       ),
-//       (
-//         CrateEvent::from(MemberEvent {
-//           ty: MemberEventType::Reap,
-//           members: TinyVec::new().into(),
-//         }),
-//         true,
-//       ),
-//     ];
+  #[test]
+  fn test_member_event_coalesce_pass_through() {
+    let cases = [
+      (CrateEvent::from(UserEventMessage::default()), false),
+      (
+        CrateEvent::from(MemberEvent {
+          ty: MemberEventType::Join,
+          members: TinyVec::new().into(),
+        }),
+        true,
+      ),
+      (
+        CrateEvent::from(MemberEvent {
+          ty: MemberEventType::Leave,
+          members: TinyVec::new().into(),
+        }),
+        true,
+      ),
+      (
+        CrateEvent::from(MemberEvent {
+          ty: MemberEventType::Failed,
+          members: TinyVec::new().into(),
+        }),
+        true,
+      ),
+      (
+        CrateEvent::from(MemberEvent {
+          ty: MemberEventType::Update,
+          members: TinyVec::new().into(),
+        }),
+        true,
+      ),
+      (
+        CrateEvent::from(MemberEvent {
+          ty: MemberEventType::Reap,
+          members: TinyVec::new().into(),
+        }),
+        true,
+      ),
+    ];
 
-//     for (event, handle) in cases.iter() {
-//       let coalescer = MemberEventCoalescer::<Transport, Delegate>::new();
-//       assert_eq!(coalescer.handle(event), *handle);
-//     }
-//   }
-// }
+    for (event, handle) in cases.iter() {
+      let coalescer = MemberEventCoalescer::<Transport, Delegate>::new();
+      assert_eq!(coalescer.handle(event), *handle);
+    }
+  }
+}
--- serf-core/src/coalesce/user.rs
+++ serf-core/src/coalesce/user.rs
@@ -97,141 +97,138 @@ where
   }
 }
 
-// #[cfg(all(test, feature = "test"))]
-// mod tests {
-//   use std::net::SocketAddr;
+#[cfg(all(test, feature = "test"))]
+mod tests {
+  use std::net::SocketAddr;
 
-//   use agnostic_lite::tokio::TokioRuntime;
-//   use memberlist_core::transport::resolver::socket_addr::SocketAddrResolver;
+  use agnostic_lite::tokio::TokioRuntime;
+  use memberlist_core::transport::{
+    resolver::socket_addr::SocketAddrResolver, unimplemented::UnimplementedTransport,
+  };
 
-//   use crate::{
-//     DefaultDelegate,
-//     event::{MemberEvent, MemberEventType},
-//   };
+  use crate::{
+    DefaultDelegate,
+    event::{MemberEvent, MemberEventType},
+  };
 
-//   use super::*;
+  use super::*;
 
-//   type Transport = UnimplementedTransport<
-//     SmolStr,
-//     SocketAddrResolver<TokioRuntime>,
-//
-//     TokioRuntime,
-//   >;
+  type Transport = UnimplementedTransport<SmolStr, SocketAddrResolver<TokioRuntime>, TokioRuntime>;
 
-//   type Delegate = DefaultDelegate<Transport>;
+  type Delegate = DefaultDelegate<Transport>;
 
-//   #[tokio::test]
-//   async fn test_user_event_coalesce_basic() {
-//     let (tx, rx) = async_channel::unbounded();
-//     let (_shutdown_tx, shutdown_rx) = async_channel::bounded(1);
-//     let coalescer = UserEventCoalescer::<Transport, Delegate>::new();
+  #[tokio::test]
+  async fn test_user_event_coalesce_basic() {
+    let (tx, rx) = async_channel::unbounded();
+    let (_shutdown_tx, shutdown_rx) = async_channel::bounded(1);
+    let coalescer = UserEventCoalescer::<Transport, Delegate>::new();
 
-//     let in_ = coalesced_event(
-//       tx,
-//       shutdown_rx,
-//       Duration::from_millis(20),
-//       Duration::from_millis(20),
-//       coalescer,
-//     );
+    let in_ = coalesced_event(
+      tx,
+      shutdown_rx,
+      Duration::from_millis(20),
+      Duration::from_millis(20),
+      coalescer,
+    );
 
-//     let send = vec![
-//       UserEventMessage::default()
-//         .with_name("foo".into())
-//         .with_cc(true)
-//         .with_ltime(1.into()),
-//       UserEventMessage::default()
-//         .with_name("foo".into())
-//         .with_cc(true)
-//         .with_ltime(2.into()),
-//       UserEventMessage::default()
-//         .with_name("bar".into())
-//         .with_cc(true)
-//         .with_ltime(2.into())
-//         .with_payload("test1".into()),
-//       UserEventMessage::default()
-//         .with_name("bar".into())
-//         .with_cc(true)
-//         .with_ltime(2.into())
-//         .with_payload("test2".into()),
-//     ];
+    let send = vec![
+      UserEventMessage::default()
+        .with_name("foo".into())
+        .with_cc(true)
+        .with_ltime(1.into()),
+      UserEventMessage::default()
+        .with_name("foo".into())
+        .with_cc(true)
+        .with_ltime(2.into()),
+      UserEventMessage::default()
+        .with_name("bar".into())
+        .with_cc(true)
+        .with_ltime(2.into())
+        .with_payload("test1".into()),
+      UserEventMessage::default()
+        .with_name("bar".into())
+        .with_cc(true)
+        .with_ltime(2.into())
+        .with_payload("test2".into()),
+    ];
 
-//     for event in send {
-//       in_.send(CrateEvent::from(event)).await.unwrap();
-//     }
+    for event in send {
+      in_.send(CrateEvent::from(event)).await.unwrap();
+    }
 
-//     let mut got_foo = false;
-//     let mut got_bar1 = false;
-//     let mut got_bar2 = false;
+    let mut got_foo = false;
+    let mut got_bar1 = false;
+    let mut got_bar2 = false;
 
-//     loop {
-//       futures::select! {
-//         _ = TokioRuntime::sleep(Duration::from_millis(40)).fuse() => break,
-//         event = rx.recv().fuse() => {
-//           let event = event.unwrap();
-//           match event {
-//             CrateEvent::User(e) => {
-//               match e.name().as_str() {
-//                 "foo" => {
-//                   assert_eq!(e.ltime(), 2.into(), "bad ltime for foo");
-//                   got_foo = true;
-//                 }
-//                 "bar" => {
-//                   assert_eq!(e.ltime(), 2.into(), "bad ltime for bar");
-//                   if e.payload().eq("test1".as_bytes()) {
-//                     got_bar1 = true;
-//                   }
+    loop {
+      futures::select! {
+        _ = TokioRuntime::sleep(Duration::from_millis(40)).fuse() => break,
+        event = rx.recv().fuse() => {
+          let event = event.unwrap();
+          match event {
+            CrateEvent::User(e) => {
+              match e.name().as_str() {
+                "foo" => {
+                  assert_eq!(e.ltime(), 2.into(), "bad ltime for foo");
+                  got_foo = true;
+                }
+                "bar" => {
+                  assert_eq!(e.ltime(), 2.into(), "bad ltime for bar");
+                  if e.payload().eq("test1".as_bytes()) {
+                    got_bar1 = true;
+                  }
 
-//                   if e.payload().eq("test2".as_bytes()) {
-//                     got_bar2 = true;
-//                   }
-//                 }
-//                 _ => unreachable!(),
-//               }
-//             }
-//             _ => unreachable!(),
-//           }
-//         }
-//       }
-//     }
+                  if e.payload().eq("test2".as_bytes()) {
+                    got_bar2 = true;
+                  }
+                }
+                _ => unreachable!(),
+              }
+            }
+            _ => unreachable!(),
+          }
+        }
+      }
+    }
 
-//     assert!(got_foo && got_bar1 && got_bar2, "missing events");
-//   }
+    assert!(got_foo && got_bar1 && got_bar2, "missing events");
+  }
 
-//   #[test]
-//   fn test_user_event_coalesce_pass_through() {
-//     let cases = [
-//       (CrateEvent::from(UserEventMessage::default()), false),
-//       (
-//         CrateEvent::from(UserEventMessage::default().with_cc(true)),
-//         true,
-//       ),
-//       (
-//         CrateEvent::from(MemberEvent {
-//           ty: MemberEventType::Join,
-//           members: TinyVec::new().into(),
-//         }),
-//         false,
-//       ),
-//       (
-//         CrateEvent::from(MemberEvent {
-//           ty: MemberEventType::Leave,
-//           members: TinyVec::new().into(),
-//         }),
-//         false,
-//       ),
-//       (
-//         CrateEvent::from(MemberEvent {
-//           ty: MemberEventType::Failed,
-//           members: TinyVec::new().into(),
-//         }),
-//         false,
-//       ),
-//     ];
+  #[test]
+  fn test_user_event_coalesce_pass_through() {
+    let cases = [
+      (CrateEvent::from(UserEventMessage::default()), false),
+      (
+        CrateEvent::from(UserEventMessage::default().with_cc(true)),
+        true,
+      ),
+      (
+        CrateEvent::from(MemberEvent {
+          ty: MemberEventType::Join,
+          members: TinyVec::new().into(),
+        }),
+        false,
+      ),
+      (
+        CrateEvent::from(MemberEvent {
+          ty: MemberEventType::Leave,
+          members: TinyVec::new().into(),
+        }),
+        false,
+      ),
+      (
+        CrateEvent::from(MemberEvent {
+          ty: MemberEventType::Failed,
+          members: TinyVec::new().into(),
+        }),
+        false,
+      ),
+    ];
 
-//     let coalescer = UserEventCoalescer::<Transport, Delegate>::new();
+    let coalescer = UserEventCoalescer::<Transport, Delegate>::new();
 
-//     for (idx, (event, should_coalesce)) in cases.iter().enumerate() {
-//       assert_eq!(coalescer.handle(event), *should_coalesce, "bad: {idx}");
-//     }
-//   }
-// }
+    for (idx, (event, should_coalesce)) in cases.iter().enumerate() {
+      assert_eq!(coalescer.handle(event), *should_coalesce, "bad: {idx}");
+    }
+  }
+}
